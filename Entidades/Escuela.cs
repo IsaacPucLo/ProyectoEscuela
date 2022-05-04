50f95923d2a0743487c7d2ee8b2d6fbc96a5b4fd
Isaac Puc <dual.isaac.puc@gmail.com>
2022-05-04 18:15:34 -0500

    Creación del proyecto

---
+++ Entidades/Escuela.cs
@@ -0,0 +1,12 @@
+namespace CoreEscuela.Entidades {
+
+    class Escuela{
+        string nombre;
+        
+        public string Nombre {
+            get { return nombre; }
+
+            set { nombre = value; }
+        }
+    }
+}
