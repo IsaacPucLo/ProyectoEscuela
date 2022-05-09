27a13e96b6850cf95784658f54b6dbb5bfd414ee
Isaac Puc <dual.isaac.puc@gmail.com>
2022-05-09 10:58:18 -0500

    Crea arreglo de objetos

--- Program.cs
+++ Program.cs
@@ -7,25 +7,36 @@ namespace ProyectoEscuela
     {
         static void Main(string[] args)
         {
-
+            //Crearcion de objetos escuela
             var escuela = new Escuela("TecNM Campus Porgreso", 2010);
-
             escuela.Pais = "México";
             escuela.Ciudad = "Porgreso";
             escuela.TipoEscuela = TiposEscuela.Universidad;
 
             var escuela2 = new Escuela("UADY", 1990, TiposEscuela.Universidad, ciudad:"Merida"); //Para indicar que el parámetro 
 
-            var curso1 = new Curso(){ Nombre = "101" };
-            var curso2 = new Curso(){ Nombre = "201" };
-            var curso3 = new Curso(){ Nombre = "301" };
+            //Creacion de cursos
+            Curso[] arregloCursos = new Curso[3];
+
+            arregloCursos[0] = new Curso(){ Nombre = "101" };
+            arregloCursos[1] = new Curso(){ Nombre = "201" };
+            arregloCursos[2] = new Curso(){ Nombre = "301" };
 
            Console.WriteLine(escuela);
            Console.WriteLine(escuela2);
-           System.Console.WriteLine(curso1.Nombre + ", " + curso1.UniqueId);
-           System.Console.WriteLine(curso2.Nombre + ", " + curso2.UniqueId);
-           System.Console.WriteLine($"{curso3.Nombre} ,  {curso3.UniqueId}");
 
+           ImprimirCursos(arregloCursos);
+
+        }
+
+        private static void ImprimirCursos(Curso[] arregloCursos)
+        {
+            int contador = 0;
+            while (contador < arregloCursos.Length)
+            {
+                System.Console.WriteLine($"Nombre: {arregloCursos[contador].Nombre}, Id: {arregloCursos[contador].UniqueId}");
+                contador++;
+            }
         }
     }
 }
